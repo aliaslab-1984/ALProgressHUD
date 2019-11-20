3f887e9013e86f40e836afb5c17cff1dbd14beb6
Enrico Bonaldo <e.bonaldo@aliaslab.net>
2019-11-20 12:34:21 +0100

    Initial Commit

---
+++ ALProgressHUD/ALProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  ALProgressHUD.h
+//  ALProgressHUD
+//
+//  Created by Enrico on 20/11/2019.
+//  Copyright © 2019 AliasLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ALProgressHUD.
+FOUNDATION_EXPORT double ALProgressHUDVersionNumber;
+
+//! Project version string for ALProgressHUD.
+FOUNDATION_EXPORT const unsigned char ALProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALProgressHUD/PublicHeader.h>
+
+
